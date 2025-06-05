41ce42e307afe84db1a944cf565fa3d80c81dbf1
marifinilham04 <ailham983@gmail.com>
2025-06-05 10:40:45 +0700

    Latihan: Menambahkan potensi code smell untuk SonarCloud

--- script.js
+++ script.js
@@ -88,3 +88,13 @@ if (typeof module !== 'undefined' && module.exports) {
 module.exports = { performCalculation };
 
 }
+
+function deleteLast() {
+
+if (true) { // Kondisi yang selalu benar
+
+display.value = display.value.slice(0, -1);
+
+}
+
+}
